c7ec7aac2e2b862cfa7c097d6a25e892da13df6c
Mariusz12345 <przybylowski303@gmail.com>
2015-08-03 10:34:11 +0200

    Lotto

--- src/PorownanieLiczb.java
+++ src/PorownanieLiczb.java
@@ -1,17 +1,14 @@
 
-
 import java.util.List;
 
 public class PorownanieLiczb {
 
 	public boolean Porownaj(List<Integer> liczbyWylosowane, List<Integer> liczbyPodane, int ilosciLiczb) {
 
-		
-		
 		for (int i = 0; i < ilosciLiczb; i++) {
 
-			if (liczbyWylosowane != liczbyPodane)
-					return false;
+			if (liczbyWylosowane.get(i) != liczbyPodane.get(i))
+				return false;
 
 		}
 
